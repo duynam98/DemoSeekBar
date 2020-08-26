072216169df878ddafcc07dcfe81a59309dd67c1
toduynam <nam.toduy@tapbi.vn>
2020-08-26 08:13:08 +0700

    line

--- app/src/main/java/com/duynam/demoseekbar/SeekView.java
+++ app/src/main/java/com/duynam/demoseekbar/SeekView.java
@@ -6,7 +6,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -20,8 +20,9 @@ public class SeekView extends View {
 
     private Paint paint;
     private Paint paintRect;
+    private Paint painOval;
     private Bitmap bitmapDot;
-    private Rect rect;
+    private RectF rect;
 
     private float dotX;
     private float dotY;
@@ -35,7 +36,7 @@ public class SeekView extends View {
     private float stopX = 0;
     private float stopY = 0;
 
-    private float leftBitmap=0;
+    private float leftBitmap = 0;
 
     public SeekView(Context context) {
         super(context);
@@ -68,30 +69,34 @@ public class SeekView extends View {
         paint.setAntiAlias(true);
         bitmapDot = BitmapFactory.decodeResource(getResources(), R.drawable.dot);
         Bitmap bitmap = Bitmap.createBitmap(44, 44, Bitmap.Config.ARGB_8888);
-        rect = new Rect();
+        rect = new RectF();
+
+        painOval = new Paint();
+        painOval.setColor(Color.YELLOW);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         paint.setStrokeWidth(0.05f * getMeasuredHeight());
-        paintRect.setStrokeWidth(0.05f * getMeasuredHeight());
-        canvas.drawLine(0, 0.5f * getMeasuredHeight(), getMeasuredWidth(), 0.5f * getMeasuredHeight(), paint);
-        canvas.drawLine(0, 0.3f * getMeasuredHeight(), 0, 0.7f * getMeasuredHeight(), paint);
-        canvas.drawLine(0.25f * getMeasuredWidth(), 0.3f * getMeasuredHeight(), 0.25f * getMeasuredWidth(), 0.7f * getMeasuredHeight(), paint);
-        canvas.drawLine(0.5f * getMeasuredWidth(), 0.3f * getMeasuredHeight(), 0.5f * getMeasuredWidth(), 0.7f * getMeasuredHeight(), paint);
-        canvas.drawLine(0.75f * getMeasuredWidth(), 0.3f * getMeasuredHeight(), 0.75f * getMeasuredWidth(), 0.7f * getMeasuredHeight(), paint);
-        canvas.drawLine(0.75f * getMeasuredWidth(), 0.3f * getMeasuredHeight(), 0.75f * getMeasuredWidth(), 0.7f * getMeasuredHeight(), paint);
-        canvas.drawLine(getMeasuredWidth(), 0.3f * getMeasuredHeight(), getMeasuredWidth(), 0.7f * getMeasuredHeight(), paint);
+        paintRect.setStrokeWidth(0.07f * getMeasuredHeight());
+        canvas.drawLine(getPaddingLeft(), 0.5f * getMeasuredHeight(), getWidth() - getPaddingRight(), 0.5f * getMeasuredHeight(), paint);
+        canvas.drawLine(getPaddingLeft(), 0.3f * getMeasuredHeight(), getPaddingLeft(), 0.7f * getMeasuredHeight(), paint);
+        //canvas.drawLine(0.25f * getMeasuredWidth() - getPaddingLeft(), 0.3f * getMeasuredHeight(), 0.25f * getMeasuredWidth() - getPaddingLeft(), 0.7f * getMeasuredHeight(), paint);
+        canvas.drawLine(0.5f * getMeasuredWidth() - getPaddingLeft(), 0.3f * getMeasuredHeight(), 0.5f * getMeasuredWidth() - getPaddingLeft(), 0.7f * getMeasuredHeight(), paint);
+        canvas.drawLine(0.75f * getMeasuredWidth() - getPaddingLeft(), 0.3f * getMeasuredHeight(), 0.75f * getMeasuredWidth() - getPaddingLeft(), 0.7f * getMeasuredHeight(), paint);
+        canvas.drawLine(0.75f * getMeasuredWidth() - getPaddingLeft(), 0.3f * getMeasuredHeight(), 0.75f * getMeasuredWidth() - getPaddingLeft(), 0.7f * getMeasuredHeight(), paint);
+        canvas.drawLine(getWidth() - getPaddingRight(), 0.3f * getMeasuredHeight(), getWidth() - getPaddingRight(), 0.7f * getMeasuredHeight(), paint);
 
-//        rect.top = getMeasuredHeight() / 2 - bitmapDot.getHeight() / 2;
-//        rect.bottom = getMeasuredHeight() / 2 + bitmapDot.getHeight() / 2;
-//        rect.right = bitmapDot.getWidth();
-//        rect.left = 0;
+        rect.top = (int) (0.5f * getMeasuredHeight());
+        rect.bottom = (int) (0.5f * getMeasuredHeight());
+        rect.right = getPaddingLeft() + 44;
+        rect.left = getPaddingLeft();
 
         //canvas.drawBitmap(bitmapDot, null, rect, paint);
-        canvas.drawLine(startX, 0.5f * getMeasuredHeight(), stopX, 0.5f * getMeasuredHeight(), paintRect);
-        canvas.drawBitmap(bitmapDot, leftBitmap, 0, null);
+        //canvas.drawLine(getPaddingLeft() + startX, 0.5f * getMeasuredHeight(), stopX, 0.5f * getMeasuredHeight(), paintRect);
+        canvas.drawOval(leftBitmap+getPaddingLeft()+22, 0.3f * getMeasuredHeight(), leftBitmap+44, 0.7f * getMeasuredHeight(), painOval);
     }
 
     @Override
@@ -127,11 +132,13 @@ public class SeekView extends View {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                painOval.setColor(Color.GREEN);
                 break;
             case MotionEvent.ACTION_MOVE:
 //                FrameLayout.LayoutParams mParam = (FrameLayout.LayoutParams) this.getLayoutParams();
 //                mParam.leftMargin = (int) deltaX;
 //                mParam.rightMargin = deltaY;
+                //leftBitmap = getWidth() - getPaddingLeft() - 22;
                 dotX = event.getX();
                 dotY = event.getY();
                 dotRawX = event.getRawX();
@@ -139,15 +146,27 @@ public class SeekView extends View {
                 deltaX = event.getRawX();
                 deltaY = dotRawY - dotY;
                 stopX = event.getX();
-                leftBitmap = event.getX();
-                if (event.getX() >= getMeasuredWidth()/8 && event.getX() < (getMeasuredWidth()/8)*3){
-                    leftBitmap = getMeasuredWidth()/4;
-                    stopX = getMeasuredWidth()/4;
-                } else if (event.getX() >= (getMeasuredWidth()/8)*3){
-                    leftBitmap = getMeasuredWidth()/2;
-                    stopX = getMeasuredWidth()/2;
+                leftBitmap = leftBitmap + event.getX();
+                if (event.getX() < getPaddingLeft()) {
+                    leftBitmap = 0;
+                    stopX = 0;
                 }
+                if (event.getX() > 0.8f * getWidth() - getPaddingLeft()) {
+                    leftBitmap = getWidth() - getPaddingRight() - 22;
+                    stopX = getWidth() - getPaddingRight() + 22;
+                    //stopX = getWidth() - getPaddingRight() + 22;
+                }
+//                if (event.getX() >= getMeasuredWidth() / 8 && event.getX() < (getMeasuredWidth() / 8) * 3) {
+//                    leftBitmap = getMeasuredWidth() / 4;
+//                    stopX = getMeasuredWidth() / 4;
+//                } else if (event.getX() >= (getMeasuredWidth() / 8) * 3) {
+//                    leftBitmap = getMeasuredWidth() / 2;
+//                    stopX = getMeasuredWidth() / 2;
+//                }
                 Log.e("deltaX", "onTouchEvent: " + event.getX());
+                break;
+            case MotionEvent.ACTION_UP:
+                painOval.setColor(Color.YELLOW);
                 break;
         }
         invalidate();
